0b94cb8bb1e11fa1bdee50fa1a1aecb0a45168ba
pranabcode2023 <pranabcode2023@gmail.com>
2023-06-20 14:23:23 +0200

    first comment

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 5000;
+
+//NOTE - cors
+import cors from "cors";
+
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(cors());
+
+//NOTE - cors
+
+app.listen(port, () => {
+    console.log("Server is running on port" + port);
+});
